d05c7772ee216aa82cad0ba38552c8e0ccdd6323
omid nassir <nassiromid@gmail.com>
2019-09-02 15:06:21 -0700

    Refactored service interfaces to common interface. Closes #11.

--- pet-clinic-data/src/main/java/com/rissandimo/petclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/com/rissandimo/petclinic/services/OwnerService.java
@@ -2,15 +2,7 @@ package com.rissandimo.petclinic.services;
 
 import com.rissandimo.petclinic.model.Owner;
 
-import java.util.Set;
-
-public interface OwnerService
+public interface OwnerService extends CrudService<Owner, Long>
 {
     Owner findByLastName(String lastName);
-
-    Owner findById(Long id);
-
-    Owner saveOwner(Owner owner);
-
-    Set<Owner> findAll();
 }
--- pet-clinic-data/src/main/java/com/rissandimo/petclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/com/rissandimo/petclinic/services/PetService.java
@@ -2,13 +2,6 @@ package com.rissandimo.petclinic.services;
 
 import com.rissandimo.petclinic.model.Pet;
 
-import java.util.Set;
-
-public interface PetService
+public interface PetService extends CrudService<Pet, Long>
 {
-    Pet findById(Long id);
-
-    Pet savePet(Pet pet);
-
-    Set<Pet> findAll();
 }
