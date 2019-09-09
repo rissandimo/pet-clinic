71c6d7cade6f7ddf43669316556e4d11da703c38
omid nassir <nassiromid@gmail.com>
2019-09-09 14:05:52 -0700

    Updated Base Entity with JPA annotation. Closes #35.

--- pet-clinic-data/src/main/java/com/rissandimo/petclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/rissandimo/petclinic/model/BaseEntity.java
@@ -1,10 +1,17 @@
 package com.rissandimo.petclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+@MappedSuperclass
 public class BaseEntity implements Serializable
 {
-    //hibernate suggets to use Long - b/c can be null, primitives can't
+    //hibernate suggests to use Long - b/c can be null, primitives can't
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId()
