faa903a5c759090dda08600ae2ad54d001db4b49
omid nassir <nassiromid@gmail.com>
2019-09-09 14:47:34 -0700

    Converted Owners to JPA Entities. Closes #36.

--- pet-clinic-data/src/main/java/com/rissandimo/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/rissandimo/petclinic/model/Owner.java
@@ -1,13 +1,24 @@
 package com.rissandimo.petclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "owners")
 public class Owner extends Person
 {
+    @Column(name = "address")
     private String address;
+
+    @Column(name = "city")
     private String city;
+
+    @Column(name = "telephone")
     private String telephone;
+
+    //cascade all -> delete owner -> pets get deleted
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
     private Set<Pet> pets = new HashSet<>();
 
     public String getAddress()
